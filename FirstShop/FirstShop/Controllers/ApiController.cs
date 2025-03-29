fdc8d8d837db46ea3dc78f172032cfc99d20b848
MmdSo <goldenscorpion3@gmail.com>
2025-03-29 17:52:00 +0330

    api product

--- FirstShop/FirstShop/Controllers/ApiController.cs
+++ FirstShop/FirstShop/Controllers/ApiController.cs
@@ -2,20 +2,24 @@
 using FirstShop.Core.Services.Products.Brands;
 using FirstShop.Core.Services.Products.Product;
 using FirstShop.Core.Services.UserServices;
+using FirstShop.Core.Tools;
 using FirstShop.Core.ViewModels.Products;
 using FirstShop.Core.ViewModels.Users;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstShop.Controllers
 {
+    #region Product
     [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
         private IProductServices _productServices;
-        public ProductsController(IProductServices productServices)
+        private IMapper _mapper;
+        public ProductsController(IProductServices productServices , IMapper mapper)
         {
             _productServices = productServices;
+            _mapper = mapper;
         }
 
         public List<ProductViewModel> productsList { get; set; }
@@ -33,9 +37,88 @@ namespace FirstShop.Controllers
             return _productServices.GetProductsById(id);
         }
 
+        [HttpPost]
+        public long AddProductFromApi(long id)
+        {
+            return id;
+        }
+
+        [HttpPost("AddProductFromApiBody")]
+        public async Task<long> AddProductFromApiBody(ProductForApiViewModel product , IFormFile PImg)
+        {
+            var pr = _mapper.Map<ProductForApiViewModel, ProductViewModel>(product);
+
+            string fileName = NameGenerator.GenerateUniqCode() + Path.GetExtension(PImg.FileName);
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                await PImg.CopyToAsync(stream);
+            }
+
+            
+            pr.ProductImage = "/Images/" + fileName;
+
+            return await _productServices.AddProducts(pr , PImg);
+        }
+
+        [HttpPost("AddProductFromApiQuery")]
+        public async Task<long> AddProductFromApiQuery([FromQuery] ProductForApiViewModel product, IFormFile PImg)
+        {
+            var pr = _mapper.Map<ProductForApiViewModel, ProductViewModel>(product);
+
+            string fileName = NameGenerator.GenerateUniqCode() + Path.GetExtension(PImg.FileName);
+            string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+            using (var stream = new FileStream(imagePath, FileMode.Create))
+            {
+                await PImg.CopyToAsync(stream);
+            }
+
+
+            pr.ProductImage = "/Images/" + fileName;
+
+            return await _productServices.AddProducts(pr , PImg);
+        }
+
+        [HttpPut("EditProductFromApi")]
+        public async Task<IActionResult> EditProductFromApi( ProductForApiViewModel product, IFormFile PImg)
+        {
+            var pr = _mapper.Map<ProductForApiViewModel, ProductViewModel>(product);
+
+            
+                string fileName = NameGenerator.GenerateUniqCode() + Path.GetExtension(PImg.FileName);
+                string imagePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images", fileName);
+
+                using (var stream = new FileStream(imagePath, FileMode.Create))
+                {
+                    await PImg.CopyToAsync(stream);
+                }
+
+                pr.ProductImage = "/Images/" + fileName;
+            
+
+            await _productServices.EditProducts(pr, PImg);
+
+            return Ok();
+        }
+
+        [HttpDelete("DeleteProductFromApi")]
+        public async Task<IActionResult> DeleteProductFromApi(long id)
+        {
+            var pr = _productServices.GetProductsById(id);
+
+            if (pr == null)
+            {
+                return NotFound(new { message = "Not found !" });
+            }
+
+            await _productServices.DeleteProducts(id);
+
+            return Ok();
+        }
     }
-
-
+    #endregion
     [ApiController]
     [Route("api/[controller]")]
     public class BrandController : ControllerBase
